cdcf96a68e9a93aabc5c9cb525308601f0d17b26
Mostafa Ragab <m.ragabali@nu.edu.eg>
2025-11-01 15:09:12 +0200

    Add shared library steps and remove DockerUtils

--- src/org/mylib/DockerUtils.groovy
+++
@@ -1,9 +0,0 @@
-// src/org/mylib/DockerUtils.groovy
-package org.mylib
-
-class DockerUtils {
-    static void login(String user, String pass) {
-        sh "echo ${pass} | docker login -u ${user} --password-stdin"
-    }
-}
-
